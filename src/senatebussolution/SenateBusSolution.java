d7c2b4bd7c75c554d97d49c271dfb8926e2346b2
SaBeeNa <sabeena1217@gmail.com>
2018-02-11 13:53:55 +0530

    concurrent done - solution 2

--- src/senatebussolution/SenateBusSolution.java
+++ src/senatebussolution/SenateBusSolution.java
@@ -9,13 +9,37 @@ package senatebussolution;
  *
  * @author SaBeeNa
  */
+
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SenateBusSolution {
 
+    //private volatile static int waiting = 0;                         //how many riders are waiting
+    private static Counter waiting = new Counter(0);
+    private static Semaphore mutex = new Semaphore(1);      //
+    private static Semaphore bus = new Semaphore(0);        //signals when the bus has arrived
+    private static Semaphore boarded = new Semaphore(0);    //signals when a rider has boarded
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+ 
+        System.out.print("Enter the total number of Riders : ");
+        int n = sc.nextInt(); 
+        System.out.println(" ");
+         
+        // Demonstrate the behaviour of the Bus. this thread's run method will keep running
+        new Bus(n, waiting, mutex, bus, boarded).start();
+         
+        // We created 'n'number of riders to  demonstrate the behaviour of the riders pool 
+        for (int i = 0; i < n; i++) {
+            new Rider(i, waiting, mutex, bus, boarded).start();
+        }
     }
     
 }
